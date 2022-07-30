57eddea68e5cf5021907c8673e21eaa5b77c9789
Guilherme <guilherme.salviano@hotmail.com>
2022-07-30 15:06:49 -0300

    trying to create a component

--- js/functions.js
+++ js/functions.js
@@ -2,7 +2,7 @@ export function getFolder (id){
     const movieImg = document.querySelector(`[data-img="${id}"]`)
     const movieName = document.querySelector(`[data-name="${id}"]`)
     const movieYear = document.querySelector(`[data-year="${id}"]`)
-    const movieLikes = document.querySelector(`[data-img="${id}"]`)
+    const movieLikes = document.querySelector(`[data-likes="${id}"]`)
     const movieDuration = document.querySelector(`[data-img="${id}"]`)
     return{
         movieImg: movieImg,
--- js/index.js
+++ js/index.js
@@ -6,7 +6,6 @@ function request (){
         headers:{
             'Content-Type': 'application/json;charset=utf-8'
         }
-
     })
     
 }
@@ -17,11 +16,13 @@ function request (){
     console.log(apiResponse)
     for(let a = 1; a < 7; a++){
         const elements = getFolder(a)
+        const t = apiResponse.results[a].vote_average
         elements.movieName.innerHTML = apiResponse.results[a].original_title
-        elements.movieYear.innerHTML = apiResponse.results[a].release_date
+        elements.movieYear.innerHTML = apiResponse.results[a].release_date.slice(0,4)
         elements.movieLikes.innerHTML = apiResponse.results[a].vote_average
         elements.movieName.innerHTML = apiResponse.results[a].original_title
         elements.movieImg.src = `https://image.tmdb.org/t/p/w500${apiResponse.results[a].poster_path}`
+        elements.movieName.dataset.id = apiResponse.results[a].id
     }
 }) ()
 
