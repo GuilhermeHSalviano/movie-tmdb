5d0bd30a1691a6de82f44c45b927d295363e5d0d
Guilherme <guilherme.salviano@hotmail.com>
2022-07-29 21:09:58 -0300

    the request's been successful

--- js/index.js
+++ js/index.js
@@ -1,12 +1,17 @@
 function request (){
-    return fetch('https://api.themoviedb.org/3/movie/550?api_key=103186a9f4ef77e1f666cdd93a1fa70a', {
+    return fetch('https://api.themoviedb.org/3/discover/movie?api_key=103186a9f4ef77e1f666cdd93a1fa70a&?page=1&sort_by=popularity.desc', {
         method: 'GET',
+        headers:{
+            'Content-Type': 'application/json;charset=utf-8'
+        }
 
     })
-    .then(response =>{
-        console.log(response.json())
-        
-    })
+    
 }
 
-request()
+(async()=>{
+    const teste = await request()
+    const t2 = await teste.json()
+    console.log(t2)
+}) ()
+
